6a07c6441292699a41b2a90493323b4c7be3e9f6
Jakob Borg <jakob@kastelo.net>
2026-05-17 15:24:53 +0200

    write to $GITHUB_OUTPUT
    Signed-off-by: Jakob Borg <jakob@kastelo.net>

--- calver.go
+++ calver.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 	"os/exec"
 	"strings"
 	"time"
@@ -16,32 +17,45 @@ func main() {
 	flag.Parse()
 
 	now := time.Now().UTC()
-	curYear := now.Year() - 2000
-	curMonth := now.Month()
 
+	ver := version(prev, now)
+	fmt.Println(ver)
+
+	if out := os.Getenv("GITHUB_OUTPUT"); out != "" {
+		fd, err := os.OpenFile(out, os.O_WRONLY|os.O_APPEND, 0o644)
+		if err != nil {
+			fmt.Printf("Failed to open $GITHUB_OUTPUT: %v\n", err)
+			os.Exit(1)
+		}
+		_, _ = fmt.Fprintf(fd, "version=%s\n", ver)
+		_ = fd.Close()
+	}
+}
+
+func version(prev string, now time.Time) string {
 	if prev == "" {
 		prev, _ = cmd("git", "describe", "--abbrev=0", "--match", "v[0-9].*")
 	}
 	prev = strings.TrimLeft(prev, "vV")
 
+	curYear := now.Year() - 2000
+	curMonth := now.Month()
+
 	if prev == "" { // no previous version
-		fmt.Printf("%d.%d.0\n", curYear, curMonth)
-		return
+		return fmt.Sprintf("%d.%d.0\n", curYear, curMonth)
 	}
 
 	latestStable, err := semver.NewVersion(prev)
 	if err != nil { // unparseable version
-		fmt.Printf("%d.%d.0\n", curYear, curMonth)
-		return
+		return fmt.Sprintf("%d.%d.0\n", curYear, curMonth)
 	}
 
 	if latestStable.Major != int64(curYear) || latestStable.Minor != int64(curMonth) { // new month/year
-		fmt.Printf("%d.%d.0\n", curYear, curMonth)
-		return
+		return fmt.Sprintf("%d.%d.0\n", curYear, curMonth)
 	}
 
 	latestStable.Patch++
-	fmt.Println(latestStable)
+	return latestStable.String()
 }
 
 func cmd(name string, args ...string) (string, error) {
